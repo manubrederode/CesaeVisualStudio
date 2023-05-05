723401ac245acfe6fc93e2f7e53b7c2772a05548
manubrederode <brederode.emmanuelle@gmail.com>
2023-05-05 09:33:12 +0100

    criamos o projeto

---
+++ Banco2/ContaBancaria.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco2
+{
+    internal class ContaBancaria
+    {
+        //Definição de Propriedade
+        public int NumConta { get; } //não pode alterar.
+        public string Titular { get; set; }
+        public double Saldo { get; private set; } //pode ser acessado a partir de outra classe, mas só altera dentro da classe conta bancária.
+
+        public static double Taxa = 5.00; //taxa fixa.
+
+        //Construtores
+
+        //esse construtor permite instanciar contas bancárias apenas com o número de conta e titular.
+        public ContaBancaria(int numConta,  string titular)
+        {
+            NumConta = numConta;
+            Titular = titular;
+        }
+
+        //esse construtor permite instanciar uma conta a partir do numero de conta, titular e saldo inicial.
+        public ContaBancaria(int numConta, string titular, double saldo) : this(numConta, titular)
+        {
+            Saldo = saldo;
+        }
+
+        //Secção de Métodos
+
+        //Este método permite o depósito de dinheiro na conta.
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        //Este método permite o levantamento de dinheiro na conta.
+        public void Levantamento(double quantia)
+        {
+            Saldo -= (quantia + Taxa);
+        }
+
+        //permite o print das informações corretamente.
+        public override string ToString()
+        {
+            return "Número conta: " + NumConta + ", Titular da conta: " + Titular + ", Saldo: " + Saldo.ToString("F2") + " euros.";
+        }
+
+
+    }
+}
---
+++ Banco2/Program.cs
@@ -0,0 +1,52 @@
+﻿using Banco2;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        ContaBancaria cb;
+
+        Console.WriteLine("Indique o número da conta: ");
+        int numConta = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Indique o nome do titular da conta: ");
+        string titular = Console.ReadLine();
+
+        Console.WriteLine("Haverá depósito inicial (s/n)?: ");
+        char resposta = Convert.ToChar(Console.ReadLine());
+
+        if (resposta == 's' || resposta == 'S')
+        {
+            Console.WriteLine("Qual o valor do depósito incial? ");
+            double depositoInicial = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cb = new ContaBancaria(numConta, titular, depositoInicial);
+        }
+        else
+        {
+            cb = new ContaBancaria(numConta, titular);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Dados da conta: ");
+        Console.WriteLine(cb);
+        Console.WriteLine();
+        Console.WriteLine("Entre com o valor para depósito: ");
+        double quantia = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+        cb.Deposito(quantia);
+        Console.WriteLine("Dados da conta atualizados: " + cb);
+        Console.WriteLine();
+        Console.WriteLine("Entre com o valor para levantamento: ");
+        quantia = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+        cb.Levantamento(quantia);
+        Console.WriteLine("Dados da conta atualizados: " + cb);
+
+
+        //uso do set
+        Console.WriteLine();
+        cb.Titular = "Maria";
+        Console.WriteLine(cb);
+
+
+    }
+}
